750b100ed5d9361eea42230df1a986fbccbda127
Integral <integral@murena.io>
2023-10-14 21:28:32 +0800

    Alpha version

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,95 @@
-use serde_json::Value;
-use std::collections::HashMap;
-use std::error::Error;
-use urlencoding::encode;
+use reqwest::blocking::Client;
+use serde_json::{json, Value};
+use std::{collections::HashMap, error::Error};
 
 #[no_mangle]
 pub fn translate(
-    text: &str, // 待翻译文本
-    from: &str, // 源语言
-    to: &str,   // 目标语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    detect: &str, // 检测到的语言 (若使用 detect, 需要手动转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    text: &str,                     // Text to be translated
+    from: &str,                     // Source Language
+    to: &str,                       // Target Language
+    detect: &str,                   // Detected Language
+    needs: HashMap<String, String>, // Other arguments defined in info.json
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
-
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => return Err("requestPath not found".into()),
-    };
-
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
-    }
-
-    let plain_text = text.replace("/", "@@");
-    let encode_text = encode(plain_text.as_str());
-
     let res: Value = client
-        .get(format!("{url}/api/v1/{from}/{to}/{encode_text}"))
+        .get(format!(
+            "https://api.dictionaryapi.dev/api/v2/entries/{to}/{text}"
+        ))
         .send()?
         .json()?;
 
-    fn parse_result(res: Value) -> Option<String> {
-        let result = res.as_object()?.get("translation")?.as_str()?.to_string();
+    fn parse_result(res: Value, client: Client) -> Option<Value> {
+        let body = res.as_array()?.get(0)?.as_object()?;
+        let (phonetics, meanings) = (body.get("phonetics")?, body.get("meanings")?);
 
-        Some(result.replace("@@", "/"))
+        let (mut pronunciations, mut explanations, mut associations, mut sentence) =
+            (Vec::new(), Vec::new(), Vec::new(), Vec::new());
+
+        for item in phonetics.as_array()? {
+            let audio_url = item.get("audio")?.as_str()?;
+            if audio_url.is_empty() {
+                break;
+            }
+
+            let region = audio_url.get((audio_url.len() - 6)..(audio_url.len() - 4))?;
+
+            let symbol = match item.get("text") {
+                Some(_) => item.get("text")?.as_str()?,
+                None => "",
+            };
+
+            let voice = client.get(audio_url).send().ok()?.bytes().ok()?.to_vec();
+
+            pronunciations.push(json!({
+            "region": region,
+            "symbol": symbol,
+            "voice": voice,
+            }));
+        }
+
+        for item in meanings.as_array()? {
+            let _trait = item.get("partOfSpeech")?.as_str()?;
+            let mut explains = Vec::new();
+            let definitions = item.get("definitions")?.as_array()?;
+
+            for definition in definitions {
+                explains.push(definition.get("definition")?.as_str()?);
+
+                let example = match definition.get("example") {
+                    Some(_) => definition.get("example")?.as_str()?,
+                    None => "",
+                };
+
+                let synonyms = item
+                    .get("synonyms")?
+                    .as_array()?
+                    .into_iter()
+                    .map(|x| x.as_str().unwrap())
+                    .collect::<Vec<_>>();
+
+                associations.extend(synonyms);
+                sentence.push(json!({
+                "source": example,
+                "target": "",
+                }));
+            }
+
+            explanations.push(json!({
+            "trait": _trait,
+            "explains": explains,
+            }));
+        }
+
+        Some(json!({
+            "pronunciations": pronunciations,
+            "explanations": explanations,
+            "associations": associations,
+            "sentence": sentence,
+        }))
     }
-    if let Some(result) = parse_result(res) {
-        return Ok(Value::String(result));
+
+    if let Some(result) = parse_result(res, client) {
+        return Ok(result);
     } else {
         return Err("Response Parse Error".into());
     }
@@ -49,8 +101,7 @@ mod tests {
     #[test]
     fn try_request() {
         let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
-        let result = translate("你好 世界！", "auto", "en", "zh_cn", needs).unwrap();
+        let result = translate("hello", "en", "en", "en", needs).unwrap();
         println!("{result}");
     }
 }
